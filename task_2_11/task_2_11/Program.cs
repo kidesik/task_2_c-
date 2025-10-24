8d4d242ba0f6646bb2c5cc3ba2de5c89f8302e43
kidesik <elizafox2005@gmail.com>
2025-10-24 13:56:49 +0300

    Add files via upload

---
+++ task_2_11/task_2_11/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите объем и массу тела");
+double m = Convert.ToInt32(Console.ReadLine()); //масса
+double p = Convert.ToInt32(Console.ReadLine()); //плотность
+double v = m / p; //объем
+Console.WriteLine(v);
