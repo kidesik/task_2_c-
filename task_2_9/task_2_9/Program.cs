959ff591b83e9edb5d1325c2a273f3b7e9786958
kidesik <elizafox2005@gmail.com>
2025-10-24 13:54:43 +0300

    Add files via upload

---
+++ task_2_9/task_2_9/Program.cs
@@ -0,0 +1,16 @@
+﻿{
+    //a
+    Console.WriteLine("");
+    int x = Convert.ToInt32(Console.ReadLine());
+    int y = Convert.ToInt32(Console.ReadLine());
+    double z = 2 * x * x * x - 3.44 * x * y + 2.3 * x * x - 7.1 * y + 2;
+    Console.WriteLine(z);
+}
+{
+    //b
+    Console.WriteLine("");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    double x = 3.14*(a*a*a + b*b*b) +2.75*b*b -12.7*a -4.1;
+    Console.WriteLine(x);
+}
