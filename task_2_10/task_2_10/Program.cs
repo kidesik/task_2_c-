737722c6707038b79b58c7a847a183f6414c08a9
kidesik <elizafox2005@gmail.com>
2025-10-24 13:55:57 +0300

    Add files via upload

---
+++ task_2_10/task_2_10/Program.cs
@@ -0,0 +1,18 @@
+﻿{
+    //a
+    Console.WriteLine("Введите 2 целых числа");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int n = 2; // количество чисел
+    int result = (a + b) / n;
+    Console.WriteLine(result);
+}
+{
+    //b
+    Console.WriteLine("Введите 2 целых числа");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int n = 2;
+    double result = Math.Pow((a*b),1/n);
+    Console.WriteLine(result);
+}
