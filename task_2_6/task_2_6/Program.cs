a9565bdb1ffcec5d6434842adf5f6b2f242d9f0b
kidesik <elizafox2005@gmail.com>
2025-10-24 13:34:44 +0300

    Add files via upload

---
+++ task_2_6/task_2_6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите значени h");
+int h = Convert.ToInt32(Console.ReadLine()); //Высота точки наблюдения над поверхностью в метрах
+int R = 6350; // радиус
+int S = Convert.ToInt32(Math.Sqrt(2*R + h * h));
+Console.WriteLine(S);
