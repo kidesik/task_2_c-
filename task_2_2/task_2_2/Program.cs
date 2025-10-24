4d198c9fe9cc6c1e7898cbb1fc7fff425b89520f
kidesik <elizafox2005@gmail.com>
2025-10-24 13:53:13 +0300

    Add files via upload

---
+++ task_2_2/task_2_2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите значения a");
+int a = Convert.ToInt32(Console.ReadLine());
+double result = (Math.Pow(a, 2) + 10) / Math.Sqrt(Math.Pow(a, 2) + 1);
+Console.WriteLine("Результат:" + result);
