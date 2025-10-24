7aa7da44639438ab7fbb407c136128a29520ab1e
kidesik <elizafox2005@gmail.com>
2025-10-24 13:57:53 +0300

    Add files via upload

---
+++ task_2_12/task_2_12/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите количество населения и площадь територии");
+int people = Convert.ToInt32(Console.ReadLine()); //количество населения
+int country  = Convert.ToInt32(Console.ReadLine());// площпдь территории страны
+int v = people/country; //плотность населения
+Console.WriteLine(v);
---
+++ task_2_13/task_2_13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("ax + b = 0 a!=0, введите значения a и b");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+if (a != 0)
+{
+    double x = a / -b;
+    Console.WriteLine(x);
+}
+else if (b != 0 && a !=0) 
+{
+    Console.WriteLine("x любое число");
+}
+else
+{
+    Console.WriteLine("Error");
+}
