01b85e63cc56b9c0b3f5bea91c746543697fb7f7
kidesik <elizafox2005@gmail.com>
2025-10-24 13:54:00 +0300

    Add files via upload

---
+++ task_2_8/task_2_8/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("");
+int R = Convert.ToInt32(Console.ReadLine());
+double pi = 3.14;
+double C = 2 * pi * R;
+Console.WriteLine(C);
+double S = pi * R * R;
+Console.WriteLine(S);
