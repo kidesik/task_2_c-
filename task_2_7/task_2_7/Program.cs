4d1ea5efe3910d68c511b693f63502fddef27112
kidesik <elizafox2005@gmail.com>
2025-10-24 13:37:27 +0300

    Add files via upload

---
+++ task_2_7/task_2_7/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите длину ребра куба");
+int a = Convert.ToInt32(Console.ReadLine()); //ребро
+int v = Convert.ToInt32(a *a * a); //объем
+int P = Convert.ToInt32(4*a); //площадь стороны
+Console.WriteLine(v);
+Console.WriteLine(P);
