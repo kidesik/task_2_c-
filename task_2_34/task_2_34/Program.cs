38e4f2122368c6ad847fdcc6ea15b10558fa892d
kidesik <elizafox2005@gmail.com>
2025-10-25 15:50:48 +0300

    Add files via upload

---
+++ task_2_34/task_2_34/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите растояние");
+int S = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость первого автомобиля");
+int V_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость второго автомобиля");
+int V_2 = Convert.ToInt32(Console.ReadLine());
+int t = S / (V_1 + V_2);
+Console.WriteLine("Через сколько часов они встретятся: " + t);
---
+++ task_2_35/task_2_35/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите растояние");
+int S = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость первого автомобиля");
+int V_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость второго автомобиля");
+int V_2 = Convert.ToInt32(Console.ReadLine());
+double t = 0.5;
+double D = S +(V_1 - V_2) * t;
+Console.WriteLine("Растояние между автомобилями через 30 минут" + D);
